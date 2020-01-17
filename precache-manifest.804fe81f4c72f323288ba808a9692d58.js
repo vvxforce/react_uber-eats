e87936c026bbb57055a6156a64c5c7349b7fc338
vvxforce <vvxforce@gmail.com>
2020-01-17 06:15:30 +0200

    Updates

---
+++ precache-manifest.804fe81f4c72f323288ba808a9692d58.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bde1920358b38453363dfabf3527519",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "303853b0414359b41e8f",
+    "url": "/react_uber-eats/static/js/2.5e2875a3.chunk.js"
+  },
+  {
+    "revision": "e153f38a97541b0e1516",
+    "url": "/react_uber-eats/static/js/main.1c507bce.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.4831411afdd49c016f287f8976f8d4d0.js"
+  "/react_uber-eats/precache-manifest.804fe81f4c72f323288ba808a9692d58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_uber-eats/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
